d1c90a49ff3b6c10d4cb57b561b0948befd0c84c
ealc0804 <lopezerikacastillo@gmail.com>
2025-11-14 15:04:31 -0500

    Tarea completada

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+# Suma todos los números de la lista
+def addmultiplenumbers(numbers):
+    return sum(numbers)
+
+# Multiplica cada número con el que sigue.
+def multiplymultiplenumbers(numbers):
+    result = numbers[0]
+    for i in range(1, len(numbers)):
+        result *= numbers[i]
+    return result
+
+# Dice si un número es par.
+def isiteven(num):
+    return isinstance(num, int) and num % 2 == 0
+
+# Dice si un número es un entero.
+def isitaninteger(num):
+    return isinstance(num, int)
+
 def main():
   print("Hello learners!")
+
+  print(addmultiplenumbers([1, 2, 3]))
+  print(multiplymultiplenumbers([2, 3, 4]))
+  print(isiteven(8))
+  print(isitaninteger(5))
 
 if __name__=="__main__":
   main()
